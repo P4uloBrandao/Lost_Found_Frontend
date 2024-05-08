0683bef3afb7f89774c9db32d508016d8c5947c0
BornSinger7 <94750207+BornSinger7@users.noreply.github.com>
2024-05-08 18:06:53 +0100

    Profile Menu Component
    em construçao

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import './index.css';
 import { AuthProvider }  from './components/AuthContext'
 import ProtectedRoute from './components/protectedRoutes'
 import ProfileSettings from './components/profileSettings/index'
+import ProfileMenu from './components/profileMenu/index'
 import {Cloudinary} from "@cloudinary/url-gen";
 import LostObjects from './components/lostObjForm/index'
 import Navbar from './components/NavBar/index.jsx'
@@ -55,9 +56,10 @@ function App() {
             path="/profile"
             element={
               <div>
+                <ProfileMenu/>
                 <ProfileSettings />
                 <DeleteProfile />
-                
+               
               </div>
             }
           />
