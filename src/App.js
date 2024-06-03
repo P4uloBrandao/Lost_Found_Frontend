03e62ac9b0eb5a9bccd5fa3ddcfaeabe19189d72
dgvmiranda <fc58631@alunos.fc.ul.pt>
2024-06-03 20:06:41 +0100

    merge

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import styled from 'styled-components';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { AuthProvider } from "./components/AuthContext.jsx";
-
-import Layout from './components/Layout/Layout'; // Importa o Layout
-
+import React, { useContext } from "react";
 import SignUpPage from './pages/SignUpPage';
 import Home from './pages/home/index';
 import LoginPage from './pages/LoginPage';
 import ProfilePage from './pages/ProfilePageTest.jsx';
-import AddLostObjectComponent from './components/AddLostObjectComponent/index';
-import LostObjectCatalogPage from './pages/lostObjectCatalogPage.jsx';
-import RegisterLostObjectPage from './pages/RegisterLostObjectPage.jsx'; 
-import MatchObjectCatalogPage from './pages/matchObjectCatalogPage.jsx'; 
-import AdminPage from './pages/AdminPage.jsx'; 
-
-import AdminRoute from './components/routeComponent/adminRoute/index.jsx';
-import PublicRoute from './components/routeComponent/publicRoute/index.jsx';
-import PrivateRoute from './components/routeComponent/privateRoute/index.jsx';
-
-import './index.css';
-
 import styled from 'styled-components';
 import Esquadras from './components/esquadra/index'
 import DeleteProfile from './components/deleteProfile/index'
@@ -52,12 +33,6 @@ import Footer from './components/FooterComponent/index.jsx'
 import'./assets/colors/colors.css'
 import AuctionsPage from "./pages/AuctionsPage";
 const AppContainer = styled.div`
-  width: 100vw;
-  height: 100vh;
-  background-color: #f5f5f5;
-  overflow-y: auto;
-  overflow-x: hidden;
-`;
     
     background-color: var(--white-color);
     transition: background 0.2s linear;
@@ -77,32 +52,13 @@ const ContentContainer = styled.div`
 `
 
 function App() {
-  return (
-    <AppContainer>
   // const { role } = useContext(AuthContext); // Use o contexto de autenticação para acessar o papel do usuário
 
     return (
       <AppContainer>
       <AuthProvider> {/* Envolve seus componentes com o AuthProvider */}
-        <GoogleOAuthProvider clientId="535834422242-dfvm3g9s3dv6hpob73povmrmgqbmiuha.apps.googleusercontent.com">
+        <GoogleOAuthProvider clientId = "535834422242-dfvm3g9s3dv6hpob73povmrmgqbmiuha.apps.googleusercontent.com">
           <Router>
-            <Routes>
-              <Route path="/login" element={<PublicRoute><LoginPage /></PublicRoute>} />
-              <Route path="/signup" element={<PublicRoute><SignUpPage /></PublicRoute>} />
-              <Route path="*" element={
-                <Layout>
-                  <Routes>
-                    <Route path="/profile" element={<PrivateRoute><ProfilePage /></PrivateRoute>} />
-                    <Route path="/AdminPage" element={<AdminRoute><AdminPage /></AdminRoute>} />
-                    <Route path="/addFoundObject" element={<PrivateRoute><AddLostObjectComponent /></PrivateRoute>} />
-                    <Route path="/matchObjects" element={<PrivateRoute><MatchObjectCatalogPage /></PrivateRoute>} />
-                    <Route path="/addLostObject" element={<PrivateRoute><RegisterLostObjectPage /></PrivateRoute>} />
-                    <Route path="/myLostObjects" element={<PrivateRoute><LostObjectCatalogPage /></PrivateRoute>} />
-                    <Route path="/" element={<PublicRoute><Home /></PublicRoute>} />
-                  </Routes>
-                </Layout>
-              } />
-            </Routes>
             <Navbar />
             <ContentContainer>
               <Routes>
@@ -125,13 +81,8 @@ function App() {
           </Router>
           {/* <Footer/> */}
         </GoogleOAuthProvider>
+
       </AuthProvider>
-    </AppContainer>
-  );
-}
-
-export default App;
-
       </AppContainer>
     );
   }
