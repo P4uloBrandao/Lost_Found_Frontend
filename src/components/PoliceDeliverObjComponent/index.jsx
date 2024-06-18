f2e2f8aeeeb3847d3979f767a5aaa268c13a4d9e
BornSinger7 <94750207+BornSinger7@users.noreply.github.com>
2024-06-18 16:57:57 +0100

    Req 16 - policia entrega obj DONE

--- src/components/PoliceDeliverObjComponent/index.jsx
+++ src/components/PoliceDeliverObjComponent/index.jsx
@@ -38,74 +38,78 @@ const UserName = styled.div`
 export default function PoliceDeliverObjComponent() {
     const [errorMessage, setErrorMessage] = useState("");
     const [statusMessage, setStatusMessage] = useState("");
+    const [userNif, setUserNif] = useState('');
+    const [lostId, setLostId] = useState('');
+    const [foundId, setFoundId] = useState('');
+
     const [errorMessage2, setErrorMessage2] = useState("");
     const [statusMessage2, setStatusMessage2] = useState("");
-    const [userId, setUserId] = useState('');
-    const [userId2, setUserId2] = useState('');
-    const [userName, setUserName] = useState('');
-    const [userName2, setUserName2] = useState('');
+    const [userNif2, setUserNif2] = useState('');
+    const [auctionId, setAuctionId] = useState('');
+   
 
-    useEffect(() => {
-        if (userId) {
-            axios.get(`http://localhost:3000/api/users/userData/${userId}`)
-                .then(response => {
-                    setUserName(response.data.currentUser.first_name + " "+response.data.currentUser.last_name );
-                })
-                .catch(error => {
-                    setUserName('');
-                    console.error("There was an error fetching the user details!", error);
-                });
-        }
-    }, [userId]);
 
-    useEffect(() => {
-        if (userId2) {
-            axios.get(`http://localhost:3000/api/users/userData/${userId2}`)
-                .then(response => {
-                    setUserName2(response.data.currentUser.first_name + " "+response.data.currentUser.last_name );
-                })
-                .catch(error => {
-                    setUserName2('');
-                    console.error("There was an error fetching the user details!", error);
-                });
-        }
-    }, [userId2]);
-
-    const handleDeactivateSubmit = async (event) => {
+    const handleDeliverAuctionSubmit = async (event) => {
         event.preventDefault();
         try {
-            const response = await axios.put(`http://localhost:3000/api/users/deactivate/${userId}`);
-            console.log(response.data); // Log the response from the server
-            setStatusMessage("User account deactivated successfully.");
-            setErrorMessage("");
-        } catch (error) {
-            if (error.response && error.response.data) {
-                setErrorMessage(error.response.data.error);
-                setStatusMessage("");
-            } else {
-                setErrorMessage('An unexpected error occurred. Please try again.');
-                setStatusMessage("");
-            }
-        }
-    }
+          
+          const userResponse = await axios.get(`http://localhost:3000/api/users/userNif/${userNif2}`);
+          const userId2 = userResponse.data.currentUser._id;
 
-    const handleActivateSubmit = async (event) => {
+          // Step 2: Get the owner ID using the user ID
+          const bidderResponse = await axios.get(`http://localhost:3000/api/users/bidder/${userId2}`);
+          const bidderId = bidderResponse.data[0]._id;
+
+          const deliveryResponse2 = await axios.put(`http://localhost:3000/api/police/police-officers/auction/${bidderId}/${auctionId}`);
+  
+          setStatusMessage2("Object delivered successfully.");
+          setErrorMessage2("");
+        } catch (error) {
+          if (error.response && error.response.data) {
+            setErrorMessage2(error.response.data.error);
+            setStatusMessage2("");
+          } else {
+            setErrorMessage2('An unexpected error occurred. Please try again.');
+            setStatusMessage2("");
+          }
+        }
+      };
+
+      const handleDeliverFoundSubmit = async (event) => {
         event.preventDefault();
         try {
-            const response = await axios.put(`http://localhost:3000/api/users/activate/${userId2}`);
-            console.log(response.data); // Log the response from the server
-            setStatusMessage2("User account activated successfully.");
-            setErrorMessage2("");
+          
+          const userResponse = await axios.get(`http://localhost:3000/api/users/userNif/${userNif}`);
+          const userId = userResponse.data.currentUser._id;
+          
+          // Step 2: Get the owner ID using the user ID
+          const ownerResponse = await axios.get(`http://localhost:3000/api/users/owner/${userId}`);
+          const ownerId = ownerResponse.data._id;
+      
+          // Step 3: Use the owner ID and the other two IDs from inputs to call the delivery endpoint
+         
+          const deliveryResponse = await axios.put(`http://localhost:3000/api/police/police-officers/delivery/${lostId}/${foundId}/${ownerId}`);
+          
+          // Log the response from the server
+          console.log(deliveryResponse.data); 
+      
+          setStatusMessage("Object delivered successfully.");
+          setErrorMessage("");
         } catch (error) {
-            if (error.response && error.response.data) {
-                setErrorMessage2(error.response.data.error);
-                setStatusMessage2("");
-            } else {
-                setErrorMessage2('An unexpected error occurred. Please try again.');
-                setStatusMessage2("");
-            }
+          if (error.response && error.response.data) {
+            setErrorMessage(error.response.data.error);
+            setStatusMessage("");
+          } else {
+            setErrorMessage('An unexpected error occurred. Please try again.');
+            setStatusMessage("");
+          }
         }
-    }
+      };
+      
+
+  
+
+   
 
     return (
         <>
@@ -115,7 +119,7 @@ export default function PoliceDeliverObjComponent() {
                 {errorMessage && <Message error>{errorMessage}</Message>}
                 <Title>Found Object Delivery</Title>
                 <CategoryTitle>Ask for the validation email with the lost and found object's ID's and the user's NIF.</CategoryTitle>
-                {userName && <UserName>Username: {userName}</UserName>}
+               
                 <CategoryTitle></CategoryTitle>
                 <InputBox>
                     <InputF
@@ -123,8 +127,8 @@ export default function PoliceDeliverObjComponent() {
                         placeholder={'Insert Found Object Id'}
                         id="foundObjId"
                         required
-                        onChange={(e) => setUserId(e.target.value)}
-                        value={userId}
+                        onChange={(e) => setFoundId(e.target.value)}
+                        value={foundId}
                         errorMessage={'invalid'}
                         name="Found Object Id"
                     />
@@ -136,8 +140,8 @@ export default function PoliceDeliverObjComponent() {
                         placeholder={'Insert Lost Object Id'}
                         id="lostObjId"
                         required
-                        onChange={(e) => setUserId(e.target.value)}
-                        value={userId}
+                        onChange={(e) => setLostId(e.target.value)}
+                        value={lostId}
                         errorMessage={'invalid'}
                         name="Lost Object Id"
                     />
@@ -149,35 +153,51 @@ export default function PoliceDeliverObjComponent() {
                         placeholder={'Insert user NIF'}
                         id="userNif"
                         required
-                        onChange={(e) => setUserId(e.target.value)}
-                        value={userId}
+                        onChange={(e) => setUserNif(e.target.value)}
+                        value={userNif}
                         errorMessage={'invalid'}
                         name="User NIF"
                     />
                 </InputBox>
-                <InputSubmit type="submit" onClick={handleDeactivateSubmit}>Register Delivery</InputSubmit>
+                <InputSubmit type="submit" onClick={handleDeliverFoundSubmit}>Register Delivery</InputSubmit>
             </Container>
 
             <Container>
                 {statusMessage2 && <Message>{statusMessage2}</Message>}
                 {errorMessage2 && <Message error>{errorMessage2}</Message>}
-                <Title>Activate User Account</Title>
-                <CategoryTitle>Activate account based on user's Id</CategoryTitle>
-                {userName2 && <UserName>User Name: {userName2}</UserName>}
+                <Title>Auctioned Object Delivery</Title>
+                <CategoryTitle>Ask for the validation email with the auction's ID's and the user's NIF.</CategoryTitle>
+               
+                <CategoryTitle></CategoryTitle>
                 <InputBox>
                     <InputF
                         type={'text'}
-                        placeholder={'Insert user Id'}
-                        id="userId2"
+                        placeholder={'Insert Auction Id'}
+                        id="auctionId"
                         required
-                        onChange={(e) => setUserId2(e.target.value)}
-                        value={userId2}
+                        onChange={(e) => setAuctionId(e.target.value)}
+                        value={auctionId}
                         errorMessage={'invalid'}
-                        name="User Id"
+                        name="Auction Id"
                     />
                 </InputBox>
-                <InputSubmit type="submit" onClick={handleActivateSubmit}>Activate</InputSubmit>
+                <CategoryTitle></CategoryTitle>
+                <InputBox>
+                    <InputF
+                        type={'text'}
+                        placeholder={'Insert user NIF'}
+                        id="userNif2"
+                        required
+                        onChange={(e) => setUserNif2(e.target.value)}
+                        value={userNif2}
+                        errorMessage={'invalid'}
+                        name="User NIF"
+                    />
+                </InputBox>
+                <InputSubmit type="submit" onClick={handleDeliverAuctionSubmit}>Register Delivery</InputSubmit>
             </Container>
+
+           
         </>
     );
 }
