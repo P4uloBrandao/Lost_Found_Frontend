eef32c07a1f0af7691bcfd4c8ccdfb0670f7de65
Dong2196 <grcdomingues21@gmail.com>
2024-06-20 10:25:18 +0100

    update steps found

--- src/components/AddFoundObjectComponent/index.js
+++ src/components/AddFoundObjectComponent/index.js
@@ -141,7 +141,14 @@ const [policeId, setPoliceId] = useState('');
 
   let counter = 0; // Mantido no escopo do componente pai
 
-
+  const [formStepsNum, setFormStepsNum] = useState(0);
+  const formSteps = [
+      { title: "1" },
+      { title: "2" },
+      { title: "3" },
+      { title: "4" },
+      { title: "5" }
+  ];
 
   // Validations
   const [titleError, setTitleError ] = useState(false);
@@ -296,6 +303,7 @@ setLoading(false)
           nic,});
         setUserWhoFound(response.data._id)
         setUser("true")
+      nextStep()
         setTimeout(() => {
           setUser("");
       }, 3000);
@@ -387,7 +395,17 @@ const handleCategoryChange = (index, category) => {
       return { ...prevItems, [newIndex]: item };
     });
   };
-  
+  const nextStep =  () => {
+    
+    setFormStepsNum(prevStep => prevStep + 1);
+    console.log(formStepsNum)
+ };
+
+const prevStep = () => {
+    setFormStepsNum(prevStep => prevStep - 1);
+    console.log(formStepsNum)
+
+};
 useEffect(() => {
   console.log(items)
 }, [items,user]);
@@ -409,202 +427,191 @@ if (loading) {
       value="option1" />
   </RadioBtn>;
 
-  return ( <>
-  {popupMessage && <PopupAlert message={popupMessage}   />}
-  {user === "true" && (<>  <PopupAlert message={"Valid User"} /></>)}
-  {user === "false" && (<>  <PopupAlert message={"Invalid User"} /></>)}
-  <Container>
-  <Title>Finder’s Information</Title>
-      <CategoryTitle>Does the finder have an BIDFIND.er account? 
-      </CategoryTitle>
-      {newLocal}
-      {selectedValue !== "no" && (<>
-      <InputBox style={{marginTop:'15px'}}>
-        <InputF 
-        type={'number'} 
-        placeholder={'Insert your NIC'}  
-        id="nic"
-        required
-        onChange={(e) => setNic(e.target.value)}
-        value={nic}
-        
-        errorMessage={'invalid'}
-        name="NIC"/>
-
- 
-        </InputBox>
-        <InputBox>
-        <InputF 
-        type={'text'} 
-        placeholder={'Insert your Email'}  
-        id="email"
-        required
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-        
-        errorMessage={'invalid'}
-        name="Email"/>
-
- 
-        </InputBox>
-        <InputSubmit type="submit" onClick={handleUserValidation}>
-            User validation
-          </InputSubmit></>
-        )}
-      {selectedValue !== "yes" && (<>
-      <ProfileCreationComponent setUserWhoFound ={setUserWhoFound} /></>)}
-  </Container>
-  
-  {userWhoFound !== null && (
-        
-        <>
-       
-    <Container>
-      <Title>Found Object Identification</Title>
-      <InputBox>
-        <InputF 
-        type={'text'} 
-        placeholder={'Insert title of lost object'}  
-        id="title"
-        required
-        onChange={(e) => setTitle(e.target.value)}
-        value={title}
-        errorValidation={titleError}
-        errorMessage={'Title is required'}
-        name="Title"/>
-
- 
-        </InputBox>
-        
- 
-        <Title>Write the price of the lost object.</Title>
-      <InputBox>
-        <InputF
-         icon = {<EuroSymbolIcon/>}
-        type={'number'} 
-        placeholder={'Insert the price of lost object'}  
-        id="price"
-        required
-        onChange={(e) => setObjPrice(parseInt(e.target.value))}
-        value={price}        
-        errorMessage={'Price is required'}
-         errorValidation={priceError}
-        name="Price"/>
-
- 
-        </InputBox>
-    </Container>
-<Container>
-{/* //COMPONENTE PARA ADICIONAR CATEGORIAS  */}
-<Grid item xs={12} sm={12}> 
-      <Title>In what category does it fit in?</Title>
-      </Grid>
-    <AddCategoryContainer>
-        <AddCategory  
-          index={0} 
-          addItem={addItem}
-          removeCategory={removeCategory} 
-          onCategoryChange={handleCategoryChange}
-          existCategory={false}
-          objectCategories={objectCategories}
-          setObjectCategories={setObjectCategories}
-          
-
-          />
-          {components.map((_, index) => (
-            <AddCategory 
-            addItem={addItem} 
-            index={index +1} 
-            mainCategory={category}
-            removeCategory={removeCategory} 
-            onCategoryChange={handleCategoryChange}
-            existCategory={category ? true : false}
-            objectCategories={objectCategories}
-            setObjectCategories={setObjectCategories}
-            
-            />
-          ))}
-          
-       {Object.keys(items).length > 0 && (
-           <AddBtn src={AddIcon} className='addBtn' onClick={addComponent}alt="add category" />
-
-)}
-
-      {categoryError && <ErrorMessage>Category is required</ErrorMessage>}
-       </AddCategoryContainer>
-
-        </Container>
-      <Container>
-      <Title>How does it look?</Title>
-      <CategoryTitle>Upload pictures of the lost object.
-        If you don’t have any, just select the “I don’t have pictures” option.</CategoryTitle>
-        <InputBox>
-
-        <CustomInputFiles singleImage max={10}
-        onChange={onImageUpload}></CustomInputFiles>
-        </InputBox>
-
-        {imageError && <ErrorMessage>Image is required</ErrorMessage>}
-        <Title>How does it look?</Title>
-              <CategoryTitle>Upload pictures of the lost object.
-        If you don’t have any, just select the “I don’t have pictures” option.</CategoryTitle>
-              
-        <InputBox>
-        <StyledTextArea 
-        
-         type={'text'} 
-        placeholder={'Describe your object..'}  
-        id="objDesc"
-        required
-        onChange={(e) => setObjDesc(e.target.value)}
-        value={description}
-        name="Description"
-        height={80} />
-       </InputBox>
-        {descriptionError===true && <ErrorMessage>Description is required</ErrorMessage>}
-      </Container>
-        <Container>
-        <Title>Where did you lose it?</Title>
-        <CategoryTitle>Insert the location. If you don’t know, please choose the “I have no clue” option.</CategoryTitle>
-      <InputBox>
-        <InputF 
-        type={'text'} 
-        placeholder={'Insert location'}  
-        id="location"
-        required
-        onChange={(e) => setObjLoc(e.target.value)}
-        value={location}
-        
-        errorMessage={'Location is required'}
-        errorValidation={locationError}
-        name="Location"/>
-
- 
-        </InputBox>
-        <InputBox>
-        <GoogleMap
-        mapContainerStyle={mapContainerStyle}
-        zoom={10}
-        center={mapCenter}
-        onClick={handleMapClick}
-      >
-        {location && <Marker position={location} />}
-      </GoogleMap>
-      {/* Display selected location coordinates (optional) */}
-      
+return (
+  <>
+    {popupMessage && <PopupAlert message={popupMessage} />}
+    {user === "true" && <PopupAlert message="Valid User" />}
+    {user === "false" && <PopupAlert message="Invalid User" />}
     
-        </InputBox>
-        </Container>
-        <div className=' btnSubmitSection'>
-       
-            <ResetButton> Reset form</ResetButton>
-          <InputSubmit onClick={handleSubmit} className="input-submit" value="Login" label="Login">Register Object </InputSubmit>
+    <div className="progress-bar">
+      <div className="progress" style={{ width: `${(formStepsNum + 1) * 20}%` }}></div>
+      {formSteps.map((step, index) => (
+        <div key={index} className={`progress-step ${index === formStepsNum ? 'active' : ''}`}></div>
+      ))}
+    </div>
+
+    {formSteps.map((step, index) => (
+      <div key={index} className={`form-step ${index === formStepsNum ? 'active' : ''}`}>
+        {step.title === "1" && (
+          <Container>
+            <Title>Finder’s Information</Title>
+            <CategoryTitle>Does the finder have an BIDFIND.er account?</CategoryTitle>
+            {newLocal}
+            {selectedValue !== "no" && (
+              <>
+                <InputBox style={{ marginTop: '15px' }}>
+                  <InputF
+                    type="number"
+                    placeholder="Insert your NIC"
+                    id="nic"
+                    required
+                    onChange={(e) => setNic(e.target.value)}
+                    value={nic}
+                    name="NIC"
+                  />
+                </InputBox>
+                <InputBox>
+                  <InputF
+                    type="text"
+                    placeholder="Insert your Email"
+                    id="email"
+                    required
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    name="Email"
+                  />
+                </InputBox>
+                <InputSubmit type="button" onClick={handleUserValidation}>
+                  User validation
+                </InputSubmit>
+              </>
+            )}
+            {selectedValue !== "yes" && <ProfileCreationComponent setUserWhoFound={setUserWhoFound} />}
+          </Container>
+        )}
+        {step.title === "2" && userWhoFound && (
+          <Container>
+            <Title>Found Object Identification</Title>
+            <InputBox>
+              <InputF
+                type="text"
+                placeholder="Insert title of lost object"
+                id="title"
+                required
+                onChange={(e) => setTitle(e.target.value)}
+                value={title}
+                errorValidation={titleError}
+                name="Title"
+              />
+            </InputBox>
+            <Title>Write the price of the lost object.</Title>
+            <InputBox>
+              <InputF
+                type="number"
+                placeholder="Insert the price of lost object"
+                id="price"
+                required
+                onChange={(e) => setObjPrice(parseInt(e.target.value))}
+                value={price}
+                errorValidation={priceError}
+                name="Price"
+              />
+            </InputBox>
+          </Container>
+        )}
+        {step.title === "3" && (
+          <Container>
+            <Title>In what category does it fit in?</Title>
+            <AddCategoryContainer>
+              <AddCategory
+                index={0}
+                addItem={addItem}
+                removeCategory={removeCategory}
+                onCategoryChange={handleCategoryChange}
+                existCategory={false}
+                objectCategories={objectCategories}
+                setObjectCategories={setObjectCategories}
+              />
+              {components.map((_, index) => (
+                <AddCategory
+                  key={index}
+                  addItem={addItem}
+                  index={index + 1}
+                  mainCategory={category}
+                  removeCategory={removeCategory}
+                  onCategoryChange={handleCategoryChange}
+                  existCategory={category ? true : false}
+                  objectCategories={objectCategories}
+                  setObjectCategories={setObjectCategories}
+                />
+              ))}
+              {Object.keys(items).length > 0 && (
+                <AddBtn src={AddIcon} className="addBtn" onClick={addComponent} alt="add category" />
+              )}
+              {categoryError && <ErrorMessage>Category is required</ErrorMessage>}
+            </AddCategoryContainer>
+          </Container>
+        )}
+        {step.title === "4" && (
+          <Container>
+            <Title>How does it look?</Title>
+            <CategoryTitle>Upload pictures of the lost object. If you don’t have any, just select the “I don’t have pictures” option.</CategoryTitle>
+            <InputBox>
+              <CustomInputFiles singleImage max={10} onChange={onImageUpload}></CustomInputFiles>
+            </InputBox>
+            {imageError && <ErrorMessage>Image is required</ErrorMessage>}
+            <Title>How does it look?</Title>
+            <CategoryTitle>Upload pictures of the lost object. If you don’t have any, just select the “I don’t have pictures” option.</CategoryTitle>
+            <InputBox>
+              <StyledTextArea
+                type="text"
+                placeholder="Describe your object.."
+                id="objDesc"
+                required
+                onChange={(e) => setObjDesc(e.target.value)}
+                value={description}
+                name="Description"
+                height={80}
+              />
+            </InputBox>
+            {descriptionError && <ErrorMessage>Description is required</ErrorMessage>}
+          </Container>
+        )}
+        {step.title === "5" && (
+          <Container>
+            <Title>Where did you lose it?</Title>
+            <CategoryTitle>Insert the location. If you don’t know, please choose the “I have no clue” option.</CategoryTitle>
+            <InputBox>
+              <InputF
+                type="text"
+                placeholder="Insert location"
+                id="location"
+                required
+                onChange={(e) => setObjLoc(e.target.value)}
+                value={location}
+                errorValidation={locationError}
+                name="Location"
+              />
+            </InputBox>
+            <InputBox>
+              <GoogleMap
+                mapContainerStyle={mapContainerStyle}
+                zoom={10}
+                center={mapCenter}
+                onClick={handleMapClick}
+              >
+                {location && <Marker position={mapCenter} />}
+              </GoogleMap>
+            </InputBox>
+            <div className="btnSubmitSection">
+      <InputSubmit onClick={handleSubmit} className="input-submit" value="Register Object">Register Object</InputSubmit>
+    <ResetButton onClick={() => window.location.reload()}>Reset form</ResetButton>
+    </div>
+          </Container>
+        )}
+       {/* Botões de navegação */}
+       {index !== 0 && (
+                <button type="button" className="btn btn-prev" onClick={prevStep}>Previous</button>
+            )}
+            {index !== formSteps.length - 1  && userWhoFound &&  (
+                <button type="button" className="btn btn-next" onClick={nextStep}>Next</button>
+            )}
+            
         </div>
-       
-       </>
-       
-      )}
-       </>
-  );
+    ))}
+
+    
+  </>
+);
 };
 
--- src/components/matchItemsComponents/LostItemDetails/LostItemDetails.jsx
+++ src/components/matchItemsComponents/LostItemDetails/LostItemDetails.jsx
@@ -5,14 +5,12 @@ import LocationOn from '@mui/icons-material/LocationOn';
 import mapsIcon from './Map-location.svg'
 import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 
-const LostItemDetails = ({status,description,location,categoy,lost_date,enddate, coordinates }) => {
-  const [dataItem, setDataItem]=useState([status,description,location,categoy,lost_date,enddate,coordinates])
+const LostItemDetails = ({ status, description, location, category, lost_date, enddate, coordinates }) => {
+  const [dataItem, setDataItem] = useState([status, description, location, category, lost_date, enddate, coordinates]);
   const [mapCenter, setMapCenter] = useState({ lat: 38.72, lng: -9.14 }); // Initial center (Lisbon)
   const libraries = ['places']; // Include places library for location search
-  const [loading, setLoading] = useState(true);
-  const [lat, setLat] = React.useState('');
-
-  const [lng, setLng] = React.useState('');
+  const [lat, setLat] = useState(null);
+  const [lng, setLng] = useState(null);
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: 'AIzaSyDPUTFHLcj71rpOYKfPwigaRF8uiOKDvWo',
     libraries,
@@ -22,96 +20,88 @@ const LostItemDetails = ({status,description,location,categoy,lost_date,enddate,
     height: '200px',
     alignSelf: 'center',
   };
+
   useEffect(() => {
     const extractLatLng = (data) => {
       const parts = data.split(',');
-      const latPart = parts[0].trim();
-      const lngPart = parts[1].trim();
+      const latPart = parts[0].trim().split(':')[1].trim();
+      const lngPart = parts[1].trim().split(':')[1].trim();
 
-      const lat = parseFloat(latPart.split(':')[1].trim());
-      const lng = parseFloat(lngPart.split(':')[1].trim());
-      setLat(lat.toString());
-      setLng(lng.toString());
-      
-      setMapCenter({ lat:lat.toString(), lng:lng.toString() });
+      const lat = parseFloat(latPart);
+      const lng = parseFloat(lngPart);
+
+      setLat(lat);
+      setLng(lng);
+      setMapCenter({ lat: lat, lng: lng });
     };
 
     if (coordinates) {
       extractLatLng(coordinates);
     }
+    console.log(coordinates);
   }, [coordinates]);
-  
-  
+  if (loadError) return <div>Error loading maps</div>;
+  if (!isLoaded) return <div>Loading maps</div>;
+
   return (
     <div className="lost-item-details">
       <div className="first_row">
-      <div className="lost-item-status">
-        <div>
-          <span className='status-title'>Status:</span>
-          <span className='status-value'>{dataItem[0]}</span>
-        </div>
-        <div>
-          <span className='matches'>10 Matches Found</span>
-        </div>
-        <div>
+        <div className="lost-item-status">
+          <div>
+            <span className='status-title'>Status:</span>
+            <span className='status-value'>{dataItem[0]}</span>
+          </div>
+          <div>
+            <span className='matches'>10 Matches Found</span>
+          </div>
+          <div>
             <span className='remove-Item'>Remove Item</span>
+          </div>
+        </div>
+        <div className="lost-item-status">
+          <div>
+            <span className='status-title'>Category:</span>
+            <span className='status-value'>{dataItem[3]}</span>
+          </div>
+          <div>
+            <span className='matches'>Lost Date: {dataItem[4]}</span>
+          </div>
+          <div>
+            <span className='remove-Item'>End Date: {dataItem[5]}</span>
+          </div>
         </div>
       </div>
-      <div className="lost-item-status">
-        <div>
-          <span className='status-title'>Category:</span>
-          <span className='status-value'>{dataItem[3]}</span>
-        </div>
-        <div>
-          <span className='matches'>Lost Date:{dataItem[4]}</span>
-        </div>
-        <div>
-            <span className='remove-Item'>End Date: {dataItem[4]}</span>
-        </div>
-      </div>
-      </div>
-
 
       <div className="lost-item-location">
         <div className='location-data'>
           <span className='location-title'>Location</span>
           <div className='location-value'>
-            <span className='location-icon'><LocationOn/></span>
+            <span className='location-icon'><LocationOn /></span>
             <span className='location-value-data'>{dataItem[2]}</span>
           </div>
           <span className='get-coordinates'>Get GPS coordinates</span>
         </div>
         <div className='location-image'>
-          {/* <img src={mapsIcon} alt="maps" /> */}
           <GoogleMap
-      mapContainerStyle={mapContainerStyle}
-      zoom={10}
-      center={mapCenter}
-    >
-      
-        <Marker
-          position={{ lat: lat.toString(), lng: lng.toString() }}
-          title={location.address}
-        />
-      
-    </GoogleMap>
+            mapContainerStyle={mapContainerStyle}
+            zoom={10}
+            center={mapCenter}
+          >
+            {lat && lng && (
+              <Marker
+                position={{ coordinates}}
+                title={dataItem[2]}
+              />
+            )}
+          </GoogleMap>
         </div>
-      
       </div>
 
-
-        <div className="lost-item-description">
-          <span className='description-title'>Description:</span>
-          <p className='description-value'>{dataItem[1]}</p>
-        </div>
-
-      
-
-
-
-      {/* Lost item location */}
-   
-  </div>
+      <div className="lost-item-description">
+        <span className='description-title'>Description:</span>
+        <p className='description-value'>{dataItem[1]}</p>
+      </div>
+    </div>
   );
 };
 
