134531b086fece715caf5d88c03d69473cac9c09
carlosbenoliel15 <57817588+carlosbenoliel15@users.noreply.github.com>
2024-06-22 02:38:06 +0100

    top 10 bids

--- src/components/SelectedAuctionComponent/ObjectDataComponent/AuctionDataContainer.jsx
+++ src/components/SelectedAuctionComponent/ObjectDataComponent/AuctionDataContainer.jsx
@@ -6,12 +6,43 @@ import { useLocation } from 'react-router-dom';
 import './style.css';
 import AuctionComponent from './AuctionDetailsComponent';
 import LoadingSpinner from '../../LoadingPage/LoadingSpinner';
+import PersonOutlineRoundedIcon from '@mui/icons-material/PersonOutlineRounded';
+import { Box } from '@mui/material';
+
  
 const AuctionInfoComponent = ({itemid}) => {
-  console.log(itemid)
-  const [selectedAuction, setselectedAuction] = useState(itemid);
+
+
+
+  const [topBids, setTopBids] = useState([]);
+  const [currentBidder, setCurrentBidder]= useState(null);
+
+  useEffect(() => {
+    // Função assíncrona para carregar as top 10 bids
+    const fetchTopBids = async () => {
+      try {
+        // Substitua 'SEU_ENDPOINT_PARA_TOP_BIDS' pelo endpoint correto da sua API
+        const response = await axios.get(process.env.REACT_APP_API_URL+`/api/auctions/{itemid.id}/bids/`);
+        const data1=response.data
+        setTopBids(data1); // Supondo que sua API retorna um array de top 10 bids
+        
+      } catch (error) {
+        console.error('Erro ao buscar top bids:', error);
+      }
+    };
+
+    fetchTopBids();
+  }, [itemid.id]); // Use itemid.id como dependência para recarregar as top bids quando itemid mudar
 
   const location = useLocation();
+
+  function hash(str) {
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+      hash = Math.imul(hash, 31) + str.charCodeAt(i) | 0;
+    }
+    return hash;
+  }
 
   return (
     <div className="lost-item-container" style={{ display: 'flex',marginLeft: '5%',flexDirection: 'column', width: '90%',marginBottom:'90px' }}>
@@ -27,6 +58,28 @@ const AuctionInfoComponent = ({itemid}) => {
         <span className='description-title'>Description:</span>
         <p className='description-value'>{itemid.description}</p>
       </div>
+      {/* Container para as top 10 bids */}
+      <div className="lost-item-description" style={{ width: '100%', marginTop: '30px', padding: '20px' }}>
+      <span className='description-title'>Top 10 Bids:</span>
+      {itemid.bids.length!== 0? (
+        <ul className='top-bids-list'>
+          {itemid.bids.slice(0, 10).reverse().map((bid, index) => {
+            const bidderHash = hash(bid.bidder);
+            const bidderColor = `hsl(${bidderHash % 360}, 50%, 50%)`;
+            return (
+              <li key={index} className='top-bid-item'>
+                <span className='rank'>{index + 1}{'º'}</span>
+                <Box sx={{ color: bidderColor }}>
+                  <PersonOutlineRoundedIcon />
+                </Box>
+                <span className='bid-value'>{bid.value} EUR</span>
+               
+              </li>
+            );
+          })}
+        </ul>
+      ) : null}
+    </div>
     </div>
   );
 };
